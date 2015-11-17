fd06d1675387db9ff235f5e8a66cceb3d511a823
whaangbuu <whaangbuu@github.com>
2015-11-17 23:13:16 +0800

    edit index.html

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/v1', api);
 
 //load index page.
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/views/index.html');
+  res.sendFile(__dirname + '/public/app/views/index.html');
 });
 
 app.listen(config.port, function(err){
@@ -36,40 +36,43 @@ app.listen(config.port, function(err){
 
 //add libraries: js, css
 app.get('/css/prism.css', function(req, res){
-    res.sendFile(__dirname + '/public/views/css/prism.css');
+    res.sendFile(__dirname + '/public/app/views/css/prism.css');
 });
 app.get('/css/ghpages-materialize.css', function(req, res){
-    res.sendFile(__dirname + '/public/views/css/ghpages-materialize.css');
+    res.sendFile(__dirname + '/public/app/views/css/ghpages-materialize.css');
 });
 app.get('/css/main.css', function(req, res){
-    res.sendFile(__dirname + '/public/views/css/main.css');
+    res.sendFile(__dirname + '/public/app/views/css/main.css');
 });
 app.get('/js/jquery.timeago.js', function(req, res){
-    res.sendFile(__dirname + '/public/views/js/jquery.timeago.js');
+    res.sendFile(__dirname + '/public/app/views/js/jquery.timeago.js');
 });
 app.get('/js/prism.js', function(req, res){
-    res.sendFile(__dirname + '/public/views/js/prism.js');
+    res.sendFile(__dirname + '/public/app/views/js/prism.js');
 });
 app.get('/js/init.js', function(req, res){
-    res.sendFile(__dirname + '/public/views/js/init.js');
+    res.sendFile(__dirname + '/public/app/views/js/init.js');
 });
 app.get('/bin/materialize.js', function(req, res){
-    res.sendFile(__dirname + '/public/views/bin/materialize.js');
+    res.sendFile(__dirname + '/public/app/views/bin/materialize.js');
 });
 //images
 app.get('/images/favicon/guia-favicon.png', function(req, res){
-    res.sendFile(__dirname + '/public/views/images/favicon/guia-favicon.png');
+    res.sendFile(__dirname + '/public/app/views/images/favicon/guia-favicon.png');
 });
 app.get('/images/parallax1.jpg', function(req, res){
-    res.sendFile(__dirname + '/public/views/images/parallax1.jpg');
+    res.sendFile(__dirname + '/public/app/views/images/parallax1.jpg');
 });
 app.get('/images/parallax2.jpg', function(req, res){
-    res.sendFile(__dirname + '/public/views/images/parallax2.jpg');
+    res.sendFile(__dirname + '/public/app/views/images/parallax2.jpg');
+});
+app.get('/images/favicon/favicon.ico', function(req, res){
+    res.sendFile(__dirname + '/public/app/views/images/favicon/favicon.ico');
 });
 //fonts
 app.get('/font/roboto/Roboto-Regular.ttf', function(req, res){
-    res.sendFile(__dirname + '/public/views/font/roboto/Roboto-Regular.ttf');
+    res.sendFile(__dirname + '/public/app/views/font/roboto/Roboto-Regular.ttf');
 });
 app.get('/font/roboto/Roboto-Light.ttf', function(req, res){
-    res.sendFile(__dirname + '/public/views/font/roboto/Roboto-Light.ttf');
+    res.sendFile(__dirname + '/public/app/views/font/roboto/Roboto-Light.ttf');
 });
