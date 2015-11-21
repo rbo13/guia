1a992d968d468a019759983beba55ab52aeb9d12
whaangbuu <whaangbuu@github.com>
2015-11-21 13:32:09 +0800

    changed to mongolab connection

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
   module.exports = {
-  //"database": "mongodb://root:password@ds053954.mongolab.com:53954/guia",
-  "database": "mongodb://localhost/guia",
+      //"database": "mongodb://localhost/guia",
+  "database": "mongodb://root:password@ds053954.mongolab.com:53954/guia",
   "port": process.env.PORT || 8080,
   "secretKey": "Bato-Bato-Pik_GUIA<3"
 };
