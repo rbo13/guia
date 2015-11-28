f02f70835d684f495752ef13dbfaef61f5d470cf
chardyy <rborofeo@gmail.com>
2015-11-29 07:29:37 +0800

    add rewards.html and rewardService

--- public/app/app.route.js
+++ public/app/app.route.js
@@ -9,16 +9,12 @@
                     controller: 'DashboardController',
                     controllerAs: 'dashboard'
                 }).
-<<<<<<< HEAD
                 when('/dashboard/rewards', {
                     templateUrl: 'app/views/admin/add_rewards.html',
                     controller: 'DashboardController',
                     controllerAs: 'dashboard'
                 }).
-                when('/dashboard/preferences', {
-=======
                 when('/dashboard/preference', {
->>>>>>> 5fd0d1f5eee02576b9422e36c2ed7a1faa23c50a
                     templateUrl: 'app/views/admin/add_preference.html',
                     controller: 'DashboardController',
                     controllerAs: 'dashboard'
